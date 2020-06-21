6f9d1d231359bec0737e56ebd7e65cbfdaa37c63
Cody C <codyicastro@gmail.com>
2020-06-21 15:28:51 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,6 @@
 # Use this space to code along with the readme
-
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+10.times do 
+  puts phrase
+end
